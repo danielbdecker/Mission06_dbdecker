70b333db3b150a488085805b595e3445102ce2df
danielbdecker <danielbdecker@gmail.com>
2023-02-13 17:52:48 -0700

    Add project files.

---
+++ Mission06_dbdecker/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission06_dbdecker.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_dbdecker.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MoviesContext blahContext { get; set; }
+        public HomeController(ILogger<HomeController> logger, MoviesContext someName)
+        {
+            _logger = logger;
+            blahContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcast()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Movie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Movie(MovieResponse ar)
+        {
+            blahContext.Add(ar);
+            blahContext.SaveChanges();
+            return View("Confirmation", ar);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission06_dbdecker/Models/MoviesContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_dbdecker.Models
+{
+    public class MoviesContext : DbContext
+    {
+        //constructor
+        public MoviesContext (DbContextOptions<MoviesContext> options) : base (options)
+        {
+
+        }
+        public DbSet<MovieResponse> responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<MovieResponse>().HasData(
+                new MovieResponse
+                {
+                    MovieID = 1,
+                    Category = "Horror",
+                    Title = "Air Bud",
+                    Year = 1997,
+                    Director = "Charles Martin Smith",
+                    Rating = "PG",
+                    Notes = "A masterpiece"
+                },
+                new MovieResponse
+                {
+                    MovieID = 2,
+                    Category = "Horror",
+                    Title = "Air Bud: Golden Receiver",
+                    Year = 1998,
+                    Director = "Richard Martin",
+                    Rating = "G",
+                    Notes = "Another masterpiece"
+                },
+                new MovieResponse
+                {
+                    MovieID = 3,
+                    Category = "Horror",
+                    Title = "Air Bud: World Pup",
+                    Year = 2000,
+                    Director = "Bill Bannerman",
+                    Rating = "G",
+                    Notes = "Yet another masterpiece"
+                }
+                );
+        }
+    }
+}
